b29bb0a6a14bd3e12b3a4a011ccd3486335b45ec
Mohamed Ashraf <ma1913920@gmail.com>
2023-05-09 13:23:19 +0300

    changing baseUrl for frontend

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: "/api/v1/restaurants",
   headers: {
     "Content-Type": "application/json",
   },
