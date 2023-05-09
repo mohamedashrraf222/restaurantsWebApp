d322f613f6a10ad82d7cb4a6fb31941d5b20f069
Mohamed Ashraf <ma1913920@gmail.com>
2023-05-09 13:27:20 +0300

    after changing the baseUrl

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "/api/v1/restaurants",
+  baseURL: "https://restaurants-web-app.onrender.com/api/v1/restaurants",
   headers: {
     "Content-Type": "application/json",
   },
